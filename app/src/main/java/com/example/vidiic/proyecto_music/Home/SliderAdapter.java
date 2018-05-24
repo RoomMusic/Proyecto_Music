2310b60fc6adc6eb17ab8e883c7a0efd327b06c2
poldominguez <pol.dominguez@hotmail.es>
2018-05-24 20:01:51 +0200

    //integracion slider e inicio

---
+++ app/src/main/java/com/example/vidiic/proyecto_music/Home/SliderAdapter.java
@@ -0,0 +1,116 @@
+package com.example.vidiic.proyecto_music.Home;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+import com.example.vidiic.proyecto_music.R;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by poldominguez on 23/5/18.
+ */
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+
+        this.context = context;
+
+    }
+
+    public int[] slide_images = {
+
+            R.drawable.share,
+            R.drawable.headphones,
+            R.drawable.eyes
+    };
+
+    public String[] slide_headings = {
+
+            "Comparte",
+            "Escucha",
+            "Descubre"
+    };
+
+    public String[] slide_desc = {
+            "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.",
+            "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.",
+            "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen."
+
+    };
+
+
+
+    @Override
+    public int getCount() {
+        return slide_headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object o) {
+        return view == (RelativeLayout) o;
+    }
+
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+
+        ImageView slideImageView = (ImageView) view.findViewById(R.id.slide_image);
+        TextView slideHeading = (TextView) view.findViewById(R.id.slide_heading);
+        TextView slideDescription = (TextView) view.findViewById(R.id.slide_desc);
+
+        slideImageView.setImageResource(slide_images[position]);
+        slideHeading.setText(slide_headings[position]);
+        slideDescription.setText(slide_desc[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+         container.removeView((RelativeLayout)object);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
