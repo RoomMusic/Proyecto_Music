8a4b82e358969ec62acf8c78681590cd3a3eed78
poldominguez <pol.dominguez@hotmail.es>
2018-05-26 20:14:49 +0200

    //push rama master login funciona todo ok

--- app/src/main/java/com/example/vidiic/proyecto_music/Login/LoginActivity.java
+++ app/src/main/java/com/example/vidiic/proyecto_music/Login/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.vidiic.proyecto_music.Login;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import com.example.vidiic.proyecto_music.R;
@@ -34,5 +37,19 @@ public class LoginActivity extends AppCompatActivity {
         rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
 
         handler.postDelayed(runnable, 2000); //2000 is the timeout for the splash
+
+        //delcarem el valor del final
+        Button btnquanta = (Button) findViewById(R.id.btnreg);
+        btnquanta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //amb aixo mirme si el boto funciona, en el android monitor sortira si lo donem al boto o no "filtrem per flx"
+                Log.d("flx", "onClick()");
+                //option + enter i importa
+                Intent intent = new Intent(LoginActivity.this,Activity_Reg.class);
+                startActivity(intent);
+            }
+        });
     }
-}
+    }
+
