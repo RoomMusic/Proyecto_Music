0f47631289cdbe9f9bfd8e1a32fb4ddaaa6ed737
poldominguez <pol.dominguez@hotmail.es>
2018-05-26 19:16:05 +0200

    //push login parte1

--- app/src/main/java/com/example/vidiic/proyecto_music/Login/LoginActivity.java
+++ app/src/main/java/com/example/vidiic/proyecto_music/Login/LoginActivity.java
@@ -1,8 +1,38 @@
 package com.example.vidiic.proyecto_music.Login;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.example.vidiic.proyecto_music.R;
+
 /**
  * Created by poldominguez on 25/5/18.
  */
 
-public class LoginActivity {
+public class LoginActivity extends AppCompatActivity {
+
+    RelativeLayout rellay1, rellay2;
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
+        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
+
+        handler.postDelayed(runnable, 2000); //2000 is the timeout for the splash
+    }
 }
