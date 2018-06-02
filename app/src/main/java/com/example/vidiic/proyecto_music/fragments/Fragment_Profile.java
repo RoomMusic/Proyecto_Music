bb8708366f0ddf867e91792723ba62bba075b578
sergi <sergio96ruiz@gmail.com>
2018-06-02 18:54:21 +0200

    mas strings y toast eliminado

--- app/src/main/java/com/example/vidiic/proyecto_music/fragments/Fragment_Profile.java
+++ app/src/main/java/com/example/vidiic/proyecto_music/fragments/Fragment_Profile.java
@@ -61,8 +61,12 @@ public class Fragment_Profile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fragment__profile, container, false);
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment__profile, container, false);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
